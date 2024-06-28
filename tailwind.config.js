136a710cb8c4d3cf709f5e3f0062ce4479252076
Jez Emery <2809847+jezemery@users.noreply.github.com>
2024-06-28 22:19:34 +0100

    Add organisers and sponsors sections to website
    The team and sponsors sections were added to the website's home page, including specific organiser information with customised background images. The safeList in the tailwind configuration file was updated with new classes and patterns to allow for these changes. The test database URL was also modified.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,19 @@ module.exports = {
         "./assets/**/*.js",
         "./templates/**/*.html.twig",
     ],
+    safelist: [
+        {
+            pattern: /bg-organiser-+/
+        },
+    ],
     theme: {
         extend: {
             backgroundImage: {
                 'hero-image': "url('/images/pexels-mikael-blomkvist-6476783.jpg')",
+                'organiser-richard': "url('/images/richard_holloway.jpg')",
+                'organiser-verity': "url('/images/verity_maton.jpg')",
+                'organiser-lauren': "url('/images/lauren_james.jpg')",
+                'organiser-jez': "url('/images/jez_emery.jpg')",
             },
             colors: {
                 'brand-grey': '#3d424a',
@@ -18,7 +27,12 @@ module.exports = {
                 sans: ['"Source Sans 3"', 'sans-serif'],
             },
         },
-
     },
+    safeList: [
+        'bg-organiser-richard',
+        {
+            pattern: /bg-organiser-(richard|verity|jez|lauren)/,
+        }
+    ],
     plugins: [],
 }
